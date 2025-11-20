9e70f5e3a98e8ecf98fbfe377c1b7399682cc1ea
André Eigenheer <eigenheer81@gmail.com>
2025-11-20 10:42:30 +0100

    chore: adjustment in boostrap import

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@
 import i18n from '././i18n/i18n.ts'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 
 import App from './App.vue'
 
